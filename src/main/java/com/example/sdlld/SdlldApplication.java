034baacc108eb3d7fb5e1bf24b37b71978473e4c
Bhaskar Jha <bhaskarjha@fico.com>
2023-12-04 10:21:58 +0530

    first commit

---
+++ src/main/java/com/example/sdlld/SdlldApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdlld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdlldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdlldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sdlld/SdlldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdlld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdlldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
